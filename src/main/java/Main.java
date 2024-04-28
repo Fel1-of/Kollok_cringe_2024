c73e046415abf2ff300209c08e10afc27b6551b1
Iwan Ryžij <teum3048@yandex.ru>
2024-04-28 15:12:48 +0300

    I'm done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
 import com.mathsystem.api.graph.GraphFactory;
+import com.mathsystem.domain.plugin.nativerealization.plugins.NumberOfConnectivityComponents;
+import com.mathsystem.domain.plugin.plugintype.GraphCharacteristic;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        NodesEqEdges nodesEqEdges = new NodesEqEdges();
+
         var testGraph = GraphFactory.loadGraphFromFile(new File("src/main/resources/graph.txt"));
-        boolean result = nodesEqEdges.execute(testGraph);
-        System.out.println(testGraph.getEdges());
-        System.out.println(result);
+
+        EdgeCriticalConnected edgeCriticalConnected = new EdgeCriticalConnected();
+        var edges = testGraph.getEdges();
+        var k = edgeCriticalConnected.execute(testGraph);
+        System.out.println(k);
+
     }
 }
